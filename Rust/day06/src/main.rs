908b59ad147b64e69791c8d7572c19b12662afe1
Martin Loesch <martin@martinloesch.net>
2023-12-07 00:21:48 +0100

    Solved day 6, part 2.

--- Rust/day06/src/main.rs
+++ Rust/day06/src/main.rs
@@ -5,20 +5,39 @@ mod model;
 fn main() {
     aoc::print_day_header(6, "Wait For It");
 
-    // let contents = io_utils::read_lines("testdata1.txt");
-    let contents = io_utils::read_lines("puzzle.txt");
-    let races = model::Race::parse_many(&contents[0], &contents[1]);
+    // let content = io_utils::read_lines("testdata1.txt");
+    let content = io_utils::read_lines("puzzle.txt");
 
-    part1(&races);
+    part1(&content);
+
+    part2(&content);
 }
 
-fn part1(races: &Vec<model::Race>) {
+fn part1(content: &Vec<String>) {
     aoc::print_part_header(1, "Product of ways to win");
 
-    let product = races
+    let races = model::Race::parse_many(&content[0], &content[1]);
+    let product1 = races
         .iter()
-        .map(|r| r.count_ways_to_win())
+        .map(|r| r.count_ways_to_win1())
+        .fold(1, |acc, num| acc * num);
+    // println!("--------------------");
+    let product2 = races
+        .iter()
+        .map(|r| r.count_ways_to_win2())
         .fold(1, |acc, num| acc * num);
 
-    println!("Product: {}", product);
+    println!("Product 1: {}", product1);
+    println!("Product 2: {}", product2);
+}
+
+fn part2(content: &Vec<String>){
+    aoc::print_part_header(2, "Way to win in long race");
+
+    let race=model::Race::parse_single_race(&content[0], &content[1]);
+    println!("Found single race: {:?}", race);
+
+    let res = race.count_ways_to_win2();
+
+    println!("Number of winning moves: {}", res);
 }
--- Rust/day06/src/model.rs
+++ Rust/day06/src/model.rs
@@ -1,8 +1,9 @@
 use aoc_utils::data_utils;
 
+#[derive(Debug)]
 pub struct Race {
-    pub time: i32,
-    pub record: i32,
+    pub time: i64,
+    pub record: i64,
 }
 
 impl Race {
@@ -14,23 +15,71 @@ impl Race {
 
         for i in 0..times.len() {
             res.push(Race {
-                time: times[i],
-                record: records[i],
+                time: i64::from(times[i]),
+                record: i64::from(records[i]),
             });
         }
 
         return res;
     }
 
-    pub fn count_ways_to_win(&self) -> i32 {
+    pub fn parse_single_race(time: &String, record: &String) -> Race {
+        let parts = time.split(":").collect::<Vec<&str>>();
+        let t = parts[1]
+            .trim()
+            .split_whitespace()
+            .collect::<Vec<&str>>()
+            .concat()
+            .to_string();
+        println!("t: {}", t);
+        let time = t.parse::<i64>().unwrap();
+
+        let parts = record.split(":").collect::<Vec<&str>>();
+        let r = parts[1]
+            .trim()
+            .split_whitespace()
+            .collect::<Vec<&str>>()
+            .concat()
+            .to_string();
+        println!("r: {}", r);
+        let record = r.parse::<i64>().unwrap();
+
+        Race { time, record }
+    }
+
+    pub fn count_ways_to_win1(&self) -> i64 {
         let mut res = 0;
         for i in 1..self.time {
             let dist = (self.time - i) * i;
-            if dist > self.record{
+            if dist > self.record {
                 res += 1;
             }
         }
 
+        // println!("  #solutions for {:?}: {}", &self, res);
+        res
+    }
+
+    pub fn count_ways_to_win2(&self) -> i64 {
+        let time = self.time as f64;
+        let root = f64::sqrt(time * time - 4.0 * (self.record as f64));
+        let ex_sol_upper = (time + root) / 2.0;
+        let ex_sol_lower = (time - root) / 2.0;
+
+        let mut lower = ex_sol_lower as i64 - 1;
+        while ((self.time - lower) * lower) <= self. record {
+            lower += 1
+        }
+
+        let mut upper = ex_sol_upper as i64 + 2;
+        while ((self.time - upper) * upper) <= self.record {
+            upper -= 1;
+        }
+
+        // println!("  solutions: {} / {}", upper, lower);
+        let res = upper - lower + 1;
+
+        // println!("  #solutions for {:?}: {}", &self, res);
         res
     }
 }
