079e7305375faaf1e665dc12ff6dc2cb097e7634
Martin Loesch <martin@martinloesch.net>
2023-12-10 16:58:58 +0100

    Solved day 7, part 2.

--- Rust/day07/src/main.rs
+++ Rust/day07/src/main.rs
@@ -6,18 +6,37 @@ fn main() {
     aoc::print_day_header(7, "Camel Cards");
 
     // let content = io_utils::read_lines("testdata1.txt");
+    // let content = io_utils::read_lines("testdata2.txt");
     let content = io_utils::read_lines("puzzle.txt");
-    let hands: Vec<model::Hand> = content.iter().map(|l| model::Hand::parse(l)).collect();
     // println!("Hands: {:?}", hands);
 
-    part1(&hands);
+    part1(&content);
+
+    part2(&content);
 }
 
-fn part1(hands: &Vec<model::Hand>) {
+fn part1(content: &Vec<String>) {
     aoc::print_part_header(1, "Sum of all winnings");
 
+    let hands: Vec<model::Hand> = content.iter().map(|l| model::Hand::parse_part1(l)).collect();
     let mut sorted_hands = hands.clone();
     sorted_hands.sort_by(|a, b| b.cmp_part1(a));
+    // println!("Sorted hands: {:?}", sorted_hands);
+
+    let mut sum = 0;
+    for i in 0..sorted_hands.len() {
+        sum += (i as i32 + 1) * sorted_hands[i].bid;
+    }
+
+    println!("Winnings sum: {}", sum);
+}
+
+fn part2(content: &Vec<String>) {
+    aoc::print_part_header(2, "Sum of all winnings with joker");
+
+    let hands: Vec<model::Hand> = content.iter().map(|l| model::Hand::parse_part2(l)).collect();
+    let mut sorted_hands = hands.clone();
+    sorted_hands.sort_by(|a, b| b.cmp_part2(a));
     // println!("Sorted hands: {:?}", sorted_hands);
 
     let mut sum = 0;
--- Rust/day07/src/model.rs
+++ Rust/day07/src/model.rs
@@ -20,12 +20,14 @@ pub struct Hand {
 }
 
 impl Hand {
-    const VALID_CARDS: [char; 13] = [
+    const VALID_CARDS_PART1: [char; 13] = [
         'A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2',
     ];
-}
 
-impl Hand {
+    const VALID_CARDS_PART2: [char; 13] = [
+        'A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2', 'J',
+    ];
+
     pub fn cmp_part1(&self, other: &Self) -> Ordering {
         if self.hand_type == other.hand_type {
             // println!("Same hand type: {:?}", self.hand_type);
@@ -35,11 +37,11 @@ impl Hand {
                 if l_cards[i] == r_cards[i] {
                     continue;
                 }
-                let l_pos = Hand::VALID_CARDS
+                let l_pos = Hand::VALID_CARDS_PART1
                     .iter()
                     .position(|&x| x == l_cards[i])
                     .unwrap();
-                let r_pos = Hand::VALID_CARDS
+                let r_pos = Hand::VALID_CARDS_PART1
                     .iter()
                     .position(|&x| x == r_cards[i])
                     .unwrap();
@@ -58,6 +60,43 @@ impl Hand {
             return self.hand_type.cmp(&other.hand_type);
         }
     }
+
+    pub fn cmp_part2(&self, other: &Self) -> Ordering {
+        if self.hand_type == HandType::None || other.hand_type == HandType::None {
+            println!("Invalid hand types found: {:?} vs. {:?}", *self, *other);
+        }
+        if self.hand_type == other.hand_type {
+            // println!("Same hand type: {:?}", self.hand_type);
+            let l_cards = self.cards.chars().collect::<Vec<char>>();
+            let r_cards = other.cards.chars().collect::<Vec<char>>();
+            for i in 0..self.cards.len() {
+                if l_cards[i] == r_cards[i] {
+                    continue;
+                }
+                let l_pos = Hand::VALID_CARDS_PART2
+                    .iter()
+                    .position(|&x| x == l_cards[i])
+                    .unwrap();
+                let r_pos = Hand::VALID_CARDS_PART2
+                    .iter()
+                    .position(|&x| x == r_cards[i])
+                    .unwrap();
+                // println!("Found positions (lPos/rPos): {} / {}", lPos, rPos);
+                if l_pos < r_pos {
+                    return Ordering::Less;
+                } else if l_pos > r_pos {
+                    return Ordering::Greater;
+                } else {
+                    println!("EERRRROOOORRR");
+                    return Ordering::Equal;
+                }
+            }
+            println!("EERRRROOOORRR");
+            return Ordering::Equal;
+        } else {
+            return self.hand_type.cmp(&other.hand_type);
+        }
+    }
 }
 
 impl PartialEq for Hand {
@@ -67,7 +106,7 @@ impl PartialEq for Hand {
 }
 
 impl Hand {
-    pub fn parse(line: &String) -> Hand {
+    pub fn parse_part1(line: &String) -> Hand {
         let tokens = line
             .split_whitespace()
             .map(|t| t.trim())
@@ -75,15 +114,15 @@ impl Hand {
         let res = Hand {
             cards: tokens[0].to_string(),
             bid: tokens[1].parse::<i32>().unwrap(),
-            hand_type: Hand::determine_type(tokens[0]),
+            hand_type: Hand::determine_type_part1(tokens[0]),
         };
 
         res
     }
 
-    pub fn determine_type(cards: &str) -> HandType {
+    pub fn determine_type_part1(cards: &str) -> HandType {
         let mut counter = HashMap::new();
-        for vc in Hand::VALID_CARDS {
+        for vc in Hand::VALID_CARDS_PART1 {
             counter.insert(vc, 0);
         }
 
@@ -107,5 +146,84 @@ impl Hand {
             5 => HandType::HighCard,
             _ => HandType::None,
         }
+    }
+
+    pub fn parse_part2(line: &String) -> Hand {
+        let tokens = line
+            .split_whitespace()
+            .map(|t| t.trim())
+            .collect::<Vec<&str>>();
+        let res = Hand {
+            cards: tokens[0].to_string(),
+            bid: tokens[1].parse::<i32>().unwrap(),
+            hand_type: Hand::determine_type_part2(tokens[0]),
+        };
+
+        res
+    }
+
+    pub fn determine_type_part2(cards: &str) -> HandType {
+        let mut counter = HashMap::new();
+        for vc in Hand::VALID_CARDS_PART2 {
+            counter.insert(vc, 0);
+        }
+
+        for c in cards.chars() {
+            *counter.get_mut(&c).unwrap() += 1;
+        }
+        // let jokers = counter.get(&'J').unwrap();
+        let jokers = counter[&'J'];
+        counter.insert('J', 0);
+        // *counter.get_mut(&'J').unwrap() = 0;
+
+        let mut stat = counter.values().filter(|&v| *v > 0).collect::<Vec<_>>();
+        // println!("Card values: {:?},   Jokers: {}", counter.values(), jokers);
+        // println!("Card statistics: {:?}", stat);
+        stat.sort_by(|a, b| b.cmp(a));
+        // println!("Sorted: {:?}", stat);
+
+        let res = match stat.len() {
+            // all jokers
+            0 => HandType::FiveOfKind,
+            1 => HandType::FiveOfKind,
+            2 => match jokers {
+                // [4, 1]
+                0 if *stat[0] == 4 => HandType::FourOfKind,
+                // [3, 2]
+                0 => HandType::FullHouse,
+                // 1 joker => [3, 1]
+                1 if *stat[0] == 3 => HandType::FourOfKind,
+                // 1 joker => [2, 2]
+                1 => HandType::FullHouse,
+                // 2 jokers => [2, 1]
+                2 => HandType::FourOfKind,
+                // 3 jokers => [1, 1]
+                3 => HandType::FourOfKind,
+                _ => {
+                    println!("ERROR");
+                    HandType::None
+                }
+            },
+            3 => match jokers {
+                // [3, 1, 1]
+                0 if *stat[0] == 3 => HandType::ThreeOfKind,
+                // [2, 2, 1]
+                0 => HandType::TwoPair,
+                // 1 joker => [2, 1, 1]
+                1 => HandType::ThreeOfKind,
+                // 2 joker => [1, 1, 1]
+                2 => HandType::ThreeOfKind,
+                _ => {
+                    println!("ERROR");
+                    HandType::None
+                }
+            },
+            4 => HandType::OnePair,
+            5 => HandType::HighCard,
+            _ => HandType::None,
+        };
+        // println!("Determined {} as {:?}", cards, res);
+
+        res
     }
 }
