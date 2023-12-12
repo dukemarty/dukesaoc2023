44de86032d2e3c0d4a70a32379bf42fcb8752d1c
Martin Loesch <martin@martinloesch.net>
2023-12-13 00:40:42 +0100

    Solved day 5, part 2.

--- Rust/day05/src/main.rs
+++ Rust/day05/src/main.rs
@@ -5,31 +5,41 @@ use aoc_utils::{aoc, io_utils};
 mod model;
 
 fn main() {
-    aoc::print_day_header(5, "If You Give A Seed A Fertilizer");
+        aoc::print_day_header(5, "If You Give A Seed A Fertilizer");
 
     // let raw_blocks = io_utils::read_emptyline_separated_blocks("TestData1.txt");
     let raw_blocks = io_utils::read_emptyline_separated_blocks("puzzle.txt");
 
     part1(&raw_blocks);
 
-    part2();
+    part2(&raw_blocks);
 }
 
 fn part1(blocks: &Vec<Vec<String>>) {
     aoc::print_part_header(1, "Lowest location");
 
-    let seeds = blocks[0][0].split(":").collect::<Vec<&str>>()[1].split_whitespace().map(|s| s.trim().parse::<i64>().unwrap()).collect::<Vec<i64>>();
+    let seeds = blocks[0][0].split(":").collect::<Vec<&str>>()[1]
+        .split_whitespace()
+        .map(|s| s.trim().parse::<i64>().unwrap())
+        .collect::<Vec<i64>>();
     println!("Seeds: {:?}", seeds);
 
-    let mappings = blocks.iter().skip(1).map(|b| model::Block::parse_map_block(b)).collect::<Vec<model::Block>>();
+    let mappings = blocks
+        .iter()
+        .skip(1)
+        .map(|b| model::Block::parse_map_block(b))
+        .collect::<Vec<model::Block>>();
     let mut all_maps = HashMap::new();
     for m in mappings.iter() {
         all_maps.insert(m.caption.clone(), m);
-        println!("Mapping: {}", m.caption);
+        // println!("Mapping: {}", m.caption);
     }
-    println!("All mappings:\n{:?}\n", all_maps);
+    // println!("All mappings:\n{:?}\n", all_maps);
 
-    let all_locations = seeds.iter().map(|s| get_location_for_seed(&all_maps, *s)).collect::<Vec<i64>>();
+    let all_locations = seeds
+        .iter()
+        .map(|s| get_location_for_seed(&all_maps, *s))
+        .collect::<Vec<i64>>();
     println!("All locations: {:?}", all_locations);
 
     let lowest_loc = all_locations.iter().min().unwrap();
@@ -43,13 +53,73 @@ fn get_location_for_seed(all_maps: &HashMap<String, &model::Block>, seed: i64) -
     res = all_maps.get("soil-to-fertilizer").unwrap().map_source(res);
     res = all_maps.get("fertilizer-to-water").unwrap().map_source(res);
     res = all_maps.get("water-to-light").unwrap().map_source(res);
-    res = all_maps.get("light-to-temperature").unwrap().map_source(res);
-    res = all_maps.get("temperature-to-humidity").unwrap().map_source(res);
-    res = all_maps.get("humidity-to-location").unwrap().map_source(res);
+    res = all_maps
+        .get("light-to-temperature")
+        .unwrap()
+        .map_source(res);
+    res = all_maps
+        .get("temperature-to-humidity")
+        .unwrap()
+        .map_source(res);
+    res = all_maps
+        .get("humidity-to-location")
+        .unwrap()
+        .map_source(res);
 
     res
 }
 
-fn part2() {
-    aoc::print_part_header(2, "bla");
+fn part2(blocks: &Vec<Vec<String>>) {
+    aoc::print_part_header(2, "Lowest location for seed ranges");
+
+    let seeds = model::Range::parse_range_pairs(&blocks[0][0]);
+    println!("Seeds: {:?}", seeds);
+
+    let mappings = blocks
+        .iter()
+        .skip(1)
+        .map(|b| model::Block::parse_map_block(b))
+        .collect::<Vec<model::Block>>();
+    let mut all_maps = HashMap::new();
+    for m in mappings.iter() {
+        all_maps.insert(m.caption.clone(), m);
+        // println!("Mapping: {}", m.caption);
+    }
+    // println!("All mappings:\n{:?}\n", all_maps);
+
+    let all_locations = get_locations_for_seed_range(&all_maps, &seeds);
+    // println!("All locations: {:?}", all_locations);
+
+    let lowest_loc = all_locations.iter().map(|r| r.from).min().unwrap();
+
+    println!("Location: {}", lowest_loc);
+}
+
+fn get_locations_for_seed_range<'a>(
+    all_maps: &'a HashMap<String, &'a model::Block>,
+    seeds: &'a Vec<model::Range>,
+) -> Vec<model::Range> {
+    let order = [
+        "seed-to-soil",
+        "soil-to-fertilizer",
+        "fertilizer-to-water",
+        "water-to-light",
+        "light-to-temperature",
+        "temperature-to-humidity",
+        "humidity-to-location",
+    ];
+    let mut res = seeds.clone();
+
+    for next_step in order {
+        // println!("---------------------------------------------");
+        // println!("Ranges before cut: {:?}", res);
+        let next_map = *all_maps.get(next_step).unwrap();
+        // println!("Relevant mapping: {:?}", next_map);
+        let inter_res1 = next_map.recut_ranges(&res);
+        // println!("Ranges after {}: {:?}", next_step, inter_res1);
+        let inter_res2 = next_map.remap_ranges(inter_res1);
+        res = inter_res2.clone();
+    }
+
+    res
 }
--- Rust/day05/src/model.rs
+++ Rust/day05/src/model.rs
@@ -1,4 +1,28 @@
+#[derive(Debug, Clone)]
+pub struct Range {
+    pub from: i64,
+    pub to: i64,
+}
 
+impl Range {
+    pub fn parse_range_pairs(line: &String) -> Vec<Range> {
+        let nums = line.split(":").collect::<Vec<&str>>()[1]
+            .split_whitespace()
+            .map(|s| s.trim().parse::<i64>().unwrap())
+            .collect::<Vec<i64>>();
+        let mut res = Vec::new();
+
+        for i in 0..nums.len() / 2 {
+            let r = Range {
+                from: nums[2 * i],
+                to: nums[2 * i] + nums[2 * i + 1] - 1,
+            };
+            res.push(r);
+        }
+
+        return res;
+    }
+}
 
 #[derive(Debug)]
 pub struct Mapping {
@@ -8,9 +32,11 @@ pub struct Mapping {
 }
 
 impl Mapping {
-
     pub fn parse_from_line(line: &String) -> Mapping {
-        let parts = line.split_whitespace().map(|t| t.trim().parse::<i64>().unwrap()).collect::<Vec<i64>>();
+        let parts = line
+            .split_whitespace()
+            .map(|t| t.trim().parse::<i64>().unwrap())
+            .collect::<Vec<i64>>();
 
         let destination = parts[0];
         let source = parts[1];
@@ -22,7 +48,6 @@ impl Mapping {
             shift: destination - source,
         }
     }
-
 }
 
 #[derive(Debug)]
@@ -32,11 +57,14 @@ pub struct Block {
 }
 
 impl Block {
-
     pub fn parse_map_block(raw_block: &Vec<String>) -> Block {
         let res = Block {
             caption: raw_block[0].split_whitespace().collect::<Vec<&str>>()[0].to_string(),
-            mappings: raw_block[1..].to_vec().iter().map(|l| Mapping::parse_from_line(l) ).collect::<Vec<Mapping>>(),
+            mappings: raw_block[1..]
+                .to_vec()
+                .iter()
+                .map(|l| Mapping::parse_from_line(l))
+                .collect::<Vec<Mapping>>(),
         };
 
         res
@@ -44,11 +72,59 @@ impl Block {
 
     pub fn map_source(&self, source: i64) -> i64 {
         for m in self.mappings.iter() {
-            if source >= m.from && source < m.from+m.length {
+            if source >= m.from && source < m.from + m.length {
                 return source + m.shift;
             }
         }
 
         source
+    }
+
+    pub fn recut_ranges(&self, ranges: &Vec<Range>) -> Vec<Range> {
+        let mut res = ranges
+            .iter()
+            .map(|r| Range {
+                from: r.from,
+                to: r.to,
+            })
+            .collect::<Vec<Range>>();
+
+        for m in self.mappings.iter() {
+            let mut next = Vec::new();
+            for r in res {
+                let mut from = r.from;
+                if r.from < m.from && m.from < r.to {
+                    next.push(Range {
+                        from: r.from,
+                        to: m.from - 1,
+                    });
+                    from = m.from;
+                }
+                if r.from < (m.from + m.length - 1) && (m.from + m.length - 1) < r.to {
+                    next.push(Range {
+                        from,
+                        to: (m.from + m.length - 1),
+                    });
+                    from = m.from + m.length;
+                }
+                next.push(Range { from, to: r.to });
+            }
+            res = next;
+        }
+
+        return res;
+    }
+
+    pub fn remap_ranges(&self, ranges: Vec<Range>) -> Vec<Range> {
+        let mut res = Vec::new();
+
+        for r in ranges {
+            res.push(Range {
+                from: self.map_source(r.from),
+                to: self.map_source(r.to),
+            });
+        }
+
+        return res;
     }
 }
