f5e07da1dafb0ce7f86d3c0ecb856bcee6a1ae7d
Martin Loesch <martin@martinloesch.net>
2023-12-27 12:38:24 +0100

    Solved day 14, part 2.

--- Rust/day14/src/main.rs
+++ Rust/day14/src/main.rs
@@ -1,4 +1,6 @@
 use aoc_utils::{aoc, io_utils};
+use md5;
+use std::collections::HashMap;
 
 mod model;
 
@@ -10,35 +12,13 @@ fn main() {
 
     part1(&map);
 
-    part2();
+    part2(&map);
 }
 
 fn part1(map: &Vec<Vec<char>>) {
     aoc::print_part_header(1, "Load on north support beam after one N tilt");
 
-    let mut tilted: Vec<Vec<char>> = map.iter().map(|l| l.clone()).collect();
-
-    for col in 0..map[0].len() {
-        let mut tr = 0;
-        for mr in 0..map.len() {
-            match map[mr][col] {
-                '#' => {
-                    tr = mr + 1;
-                }
-                'O' => {
-                    if tr < mr {
-                        tilted[tr][col] = 'O';
-                        tilted[mr][col] = '.';
-                        tr += 1;
-                    } else {
-                        tr = mr + 1;
-                    }
-                }
-                '.' => {}
-                _ => {}
-            };
-        }
-    }
+    let tilted = model::tilt_north(map);
 
     // let output: Vec<String> = tilted
     //     .iter()
@@ -53,6 +33,55 @@ fn part1(map: &Vec<Vec<char>>) {
     println!("Total load: {}", res);
 }
 
-fn part2() {
-    aoc::print_part_header(2, "");
+const TEST_CYCLES: u64 = 1000000000;
+
+fn part2(map: &Vec<Vec<char>>) {
+    aoc::print_part_header(2, "Load on north after 1000000000 cycles");
+
+    let mut cycle = map;
+    let mut next_cycle: Vec<Vec<char>>;
+    let mut seen: HashMap<md5::Digest, u64> = HashMap::new();
+    let mut round = 0u64;
+    let mut cycle_len = 0u64;
+    while true {
+        round += 1;
+        next_cycle = model::perform_full_tilt_cycle(cycle);
+        let bytes: Vec<u8> = next_cycle.concat().iter().map(|c| *c as u8).collect();
+        let digest = md5::compute(bytes);
+        // println!("Test digest: {:?}", digest);
+
+        if seen.contains_key(&digest) {
+            cycle = &next_cycle;
+            println!(
+                "Found my cycle at round {}, same as {}",
+                round,
+                seen.get(&digest).unwrap()
+            );
+            cycle_len = round - seen.get(&digest).unwrap();
+            break;
+        } else {
+            seen.insert(digest, round);
+        }
+
+        cycle = &next_cycle;
+
+        // let output: Vec<String> = next_cycle
+        //     .iter()
+        //     .map(|l| l.into_iter().collect::<String>())
+        //     .collect();
+        // for l in output {
+        //     println!("{}", l);
+        // }
+    }
+
+    let remaining_cycles = (TEST_CYCLES - round) % cycle_len;
+    println!("remaining: {}", remaining_cycles);
+
+    for _ in 0..remaining_cycles {
+        next_cycle = model::perform_full_tilt_cycle(cycle);
+        cycle = &next_cycle;
+    }
+
+    let res = model::calculate_load_in_map(cycle);
+    println!("Load on north: {}", res);
 }
--- Rust/day14/src/model.rs
+++ Rust/day14/src/model.rs
@@ -1,4 +1,3 @@
-
 pub fn calculate_load_in_map(map: &Vec<Vec<char>>) -> i32 {
     let mut res = 0;
 
@@ -13,4 +12,151 @@ pub fn calculate_load_in_map(map: &Vec<Vec<char>>) -> i32 {
     }
 
     res
+}
+
+pub fn perform_full_tilt_cycle(map: &Vec<Vec<char>>) -> Vec<Vec<char>> {
+
+    // 1st north
+    let mut tilted_north: Vec<Vec<char>> = map.iter().map(|l| l.clone()).collect();
+    for col in 0..map[0].len() {
+        let mut tr = 0;
+        for mr in 0..map.len() {
+            match map[mr][col] {
+                '#' => {
+                    tr = mr + 1;
+                }
+                'O' => {
+                    if tr < mr {
+                        tilted_north[tr][col] = 'O';
+                        tilted_north[mr][col] = '.';
+                        tr += 1;
+                    } else {
+                        tr = mr + 1;
+                    }
+                }
+                '.' => {}
+                _ => {}
+            };
+        }
+    }
+
+    // 2nd west
+    let mut tilted_west: Vec<Vec<char>> = tilted_north.iter().map(|l| l.clone()).collect();
+    for row in 0..map.len() {
+        let mut tl = 0;
+        for ml in 0..map[0].len() {
+            match tilted_north[row][ml] {
+                '#' => {
+                    tl = ml + 1;
+                }
+                'O' => {
+                    if tl < ml {
+                        tilted_west[row][tl] = 'O';
+                        tilted_west[row][ml] = '.';
+                        tl += 1;
+                    } else {
+                        tl = ml + 1;
+                    }
+                }
+                '.' => {}
+                _ => {}
+            };
+        }
+    }
+
+    // 3rd south
+    let mut tilted_south: Vec<Vec<char>> = tilted_west.iter().map(|l| l.clone()).collect();
+    for col in 0..map[0].len() {
+        let mut tr = map.len() - 1;
+        for mr in (0..map.len()).rev() {
+            match tilted_west[mr][col] {
+                '#' => {
+                    if mr > 0 {
+                        tr = mr - 1;
+                    } else {
+                        tr = 0;
+                    }
+                }
+                'O' => {
+                    if tr > mr {
+                        tilted_south[tr][col] = 'O';
+                        tilted_south[mr][col] = '.';
+                        tr -= 1;
+                    } else {
+                        if mr > 0 {
+                            tr = mr - 1;
+                        } 
+                        // else {
+                        //     tr = 0;
+                        // }
+                    }
+                }
+                '.' => {}
+                _ => {}
+            };
+        }
+    }
+
+    // 4th east
+    let mut tilted_east: Vec<Vec<char>> = tilted_south.iter().map(|l| l.clone()).collect();
+    for row in 0..map.len() {
+        let mut tl = map[0].len() - 1;
+        for ml in (0..map[0].len()).rev() {
+            match tilted_south[row][ml] {
+                '#' => {
+                    if ml > 0 {
+                        tl = ml - 1;
+                    } else {
+                        tl = 0;
+                    }
+                }
+                'O' => {
+                    if tl > ml {
+                        tilted_east[row][tl] = 'O';
+                        tilted_east[row][ml] = '.';
+                        tl -= 1;
+                    } else {
+                        if ml > 0 {
+                            tl = ml - 1;
+                        } 
+                        // else {
+                        //     tl = 0;
+                        // }
+                    }
+                }
+                '.' => {}
+                _ => {}
+            };
+        }
+    }
+
+    tilted_east
+}
+
+pub fn tilt_north(map: &Vec<Vec<char>>) -> Vec<Vec<char>> {
+    let mut tilted: Vec<Vec<char>> = map.iter().map(|l| l.clone()).collect();
+
+    for col in 0..map[0].len() {
+        let mut tr = 0;
+        for mr in 0..map.len() {
+            match map[mr][col] {
+                '#' => {
+                    tr = mr + 1;
+                }
+                'O' => {
+                    if tr < mr {
+                        tilted[tr][col] = 'O';
+                        tilted[mr][col] = '.';
+                        tr += 1;
+                    } else {
+                        tr = mr + 1;
+                    }
+                }
+                '.' => {}
+                _ => {}
+            };
+        }
+    }
+
+    tilted
 }
