8594553f47b3a441d907654c6d4b00ae3b215d78
Martin Loesch <martin@martinloesch.net>
2023-12-31 01:50:30 +0100

    Solved day 8, part 2.

--- Rust/day08/src/main.rs
+++ Rust/day08/src/main.rs
@@ -1,5 +1,5 @@
 use aoc_utils::{aoc, io_utils};
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 mod model;
 
@@ -8,6 +8,7 @@ fn main() {
 
     // let blocks = io_utils::read_emptyline_separated_blocks("testdata1.txt");
     // let blocks = io_utils::read_emptyline_separated_blocks("testdata2.txt");
+    // let blocks = io_utils::read_emptyline_separated_blocks("testdata3.txt");
     let blocks = io_utils::read_emptyline_separated_blocks("puzzle.txt");
     let instructions = blocks[0][0].chars().collect::<Vec<char>>();
     let nodes = blocks[1]
@@ -15,10 +16,24 @@ fn main() {
         .map(|l| model::Node::parse(l).unwrap())
         .collect::<Vec<model::Node>>();
     let node_map: HashMap<_, _> = nodes.iter().map(|n| (n.name.clone(), n)).collect();
-    println!("Instructions: {:?}", instructions);
-    println!("Nodes: {:?}", node_map);
+    let start_nodes: HashSet<_> = nodes
+        .iter()
+        .filter(|n| n.is_start_node())
+        .map(|n| n.name.clone())
+        .collect();
+    let end_nodes: HashSet<_> = nodes
+        .iter()
+        .filter(|n| n.is_end_node())
+        .map(|n| n.name.clone())
+        .collect();
+    // println!("Instructions: {:?}", instructions);
+    // println!("Nodes: {:?}", node_map);
+    println!("Start nodes: {:?}", start_nodes);
+    println!("End nodes  : {:?}", end_nodes);
 
     part1(&instructions, &node_map);
+
+    part2(&instructions, &node_map, &start_nodes, &end_nodes);
 }
 
 pub fn part1(instr: &Vec<char>, nodes: &HashMap<String, &model::Node>) {
@@ -41,4 +56,68 @@ pub fn part1(instr: &Vec<char>, nodes: &HashMap<String, &model::Node>) {
     }
 
     println!("#Total of steps: {}", steps);
+}
+
+pub fn part2(
+    instr: &Vec<char>,
+    nodes: &HashMap<String, &model::Node>,
+    starts: &HashSet<String>,
+    ends: &HashSet<String>,
+) {
+    aoc::print_part_header(2, "Steps to end from all");
+
+    let mut routes = model::MultiRoute::create(instr, nodes, starts, ends);
+    println!("Multi routes:\n{:?}", routes);
+
+    let mut congruences = Vec::new();
+    for s in starts.iter() {
+        let mut steps = Vec::new();
+        steps.push(model::Step {
+            node: s.clone(),
+            count: 0,
+        });
+        let mut curr = s.clone();
+        let mut curr_rounds = 0;
+        let mut seen = HashSet::new();
+        while !seen.contains(&curr) {
+            seen.insert(curr.clone());
+            let next = routes.rounds_till_end.get(&curr).unwrap();
+            curr = next.0.clone();
+            curr_rounds += next.1;
+            steps.push(model::Step {
+                node: curr.clone(),
+                count: curr_rounds,
+            });
+        }
+
+        let last_step = steps.last().unwrap();
+        let n2 = last_step.count;
+        let n1 = steps
+            .iter()
+            .filter(|s| (**s).node == last_step.node)
+            .collect::<Vec<&model::Step>>()
+            .first()
+            .unwrap()
+            .count;
+        println!("Steps for {}: {:?}", s, steps);
+        congruences.push(model::Congruence { a: n1, m: n2 - n1 });
+    }
+    println!("To solve:");
+    for congr in congruences.iter() {
+        println!("x = {} mod {}", congr.a, congr.m);
+    }
+
+    println!("For puzzle input apparently, simplified solution possible: least common multiple should be enough!");
+
+    let res = lcm(congruences.iter().map(|c| (*c).m).collect()) * (instr.len() as u64);
+    // let mut routes = starts.iter().map(|nn| model::Route {pos: nn.clone(), rounds:0}).collect::<Vec<Route>>();
+
+    // let rounds = routes.run_till_common_end() as i64;
+    // let steps = rounds * (instr.len() as i64);// (instr.len() as i32);
+
+    println!("#Total of steps: {}", res);
+}
+
+fn lcm(numbers: Vec<i32>) -> u64 {
+    numbers.iter().fold(1u64, |acc, elem| acc * (*elem as u64))
 }
--- Rust/day08/src/model.rs
+++ Rust/day08/src/model.rs
@@ -1,5 +1,9 @@
-use regex::Regex;
 use once_cell::sync::Lazy;
+use regex::Regex;
+use std::{
+    cmp,
+    collections::{HashMap, HashSet},
+};
 
 #[derive(Debug)]
 pub struct Node {
@@ -9,10 +13,10 @@ pub struct Node {
 }
 
 impl Node {
-
     pub fn parse(line: &String) -> Option<Node> {
         // AAA = (BBB, BBB)
-        static PARSE_RE: Lazy<Regex> = Lazy::new(|| Regex::new(r"(?<name>\w+) = \((?<left>\w+), (?<right>\w+)\)").unwrap());
+        static PARSE_RE: Lazy<Regex> =
+            Lazy::new(|| Regex::new(r"(?<name>\w+) = \((?<left>\w+), (?<right>\w+)\)").unwrap());
 
         let Some(caps) = PARSE_RE.captures(&line) else {
             println!("ERROR could not parse node line: <{}>", line);
@@ -25,4 +29,128 @@ impl Node {
             right: caps["right"].to_string(),
         })
     }
+
+    pub fn is_start_node(&self) -> bool {
+        self.name.chars().last().unwrap() == 'A'
+    }
+
+    pub fn is_end_node(&self) -> bool {
+        self.name.chars().last().unwrap() == 'Z'
+    }
+}
+
+#[derive(Debug)]
+pub struct Route {
+    pub pos: String,
+    pub rounds: i32,
+}
+
+#[derive(Debug)]
+pub struct MultiRoute {
+    pub routes: Vec<Route>,
+    pub rounds_till_end: HashMap<String, (String, i32)>,
+}
+
+impl MultiRoute {
+    pub fn create(
+        instr: &Vec<char>,
+        nodes: &HashMap<String, &Node>,
+        starts: &HashSet<String>,
+        ends: &HashSet<String>,
+    ) -> MultiRoute {
+        let rounds_till_end: HashMap<_, _> = starts
+            .union(ends)
+            .map(|nn| (nn.clone(), Self::rounds_till_some_end(nn, instr, nodes)))
+            .collect();
+        // println!("Rounds till end: {:?}", rounds_till_end);
+
+        MultiRoute {
+            rounds_till_end,
+            routes: starts
+                .iter()
+                .map(|nn| Route {
+                    pos: nn.clone(),
+                    rounds: 0,
+                })
+                .collect::<Vec<Route>>(),
+        }
+    }
+
+    pub fn run_till_common_end(&mut self) -> i32 {
+        while !self.finished() {
+            // at start, all are 0 => at least 1 as max
+            let current_max = cmp::max(1, self.routes.iter().map(|r| (*r).rounds).max().unwrap());
+            for i in 0..self.routes.len() {
+                // let r = &self.routes[i];
+                while self.routes[i].rounds < current_max {
+                    let (next_stop, add_rounds) = &self.rounds_till_end[&self.routes[i].pos];
+                    let new_route = Route {
+                        pos: next_stop.clone(),
+                        rounds: self.routes[i].rounds + add_rounds,
+                    };
+                    // r.pos = next_stop.clone();
+                    // r.rounds += add_rounds;
+                    self.routes[i] = new_route;
+                }
+            }
+            println!("Next round: {:?}", self.routes);
+        }
+
+        self.routes[0].rounds
+    }
+
+    // fn find_min_route(&self) -> String {
+    //     let min_route = self.routes.iter().min_by_key(|r| *r.rounds).unwrap();
+
+    //     min_route.
+    // }
+
+    fn finished(&self) -> bool {
+        let rounds = self.routes[0].rounds;
+        self.routes
+            .iter()
+            .map(|r| &(*r).pos)
+            .all(|p| p.chars().last().unwrap() == 'Z')
+            && self.routes.iter().map(|r| (*r).rounds).all(|r| r == rounds)
+    }
+
+    fn rounds_till_some_end(
+        name: &String,
+        instr: &Vec<char>,
+        nodes: &HashMap<String, &Node>,
+    ) -> (String, i32) {
+        let mut steps = 0;
+        let mut rounds = 0;
+        let mut curr = name;
+        let mut is_first = true;
+        while !nodes[curr].is_end_node() || is_first {
+            for c in instr.iter() {
+                curr = match c {
+                    'L' => &nodes[curr].left,
+                    'R' => &nodes[curr].right,
+                    _ => {
+                        println!("ERROR could not step!!!");
+                        curr
+                    }
+                };
+                steps += 1;
+            }
+            rounds += 1;
+            is_first = false;
+        }
+
+        (curr.clone(), rounds)
+    }
+}
+
+#[derive(Debug)]
+pub struct Step {
+    pub node: String,
+    pub count: i32,
+}
+
+#[derive(Debug)]
+pub struct Congruence {
+    pub a: i32,
+    pub m: i32
 }
